42d5d554288cc72b4907483696bd3db46435fa88
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-02-22 12:22:56 +0100

    New dexterity types and subscribers

---
+++ ulearn/core/browser/variables.py
@@ -0,0 +1,57 @@
+from five import grok
+from zope.component.hooks import getSite
+from zope.component import queryUtility
+from plone.registry.interfaces import IRegistry
+
+from Products.CMFCore.utils import getToolByName
+
+from mrs.max.browser.controlpanel import IMAXUISettings
+from ulearn.core.content.community import ICommunity
+
+TEMPLATE = """\
+if (!window._MAXUI) {window._MAXUI = {}; }
+window._MAXUI.username = '%(username)s';
+window._MAXUI.oauth_token = '%(oauth_token)s';
+window._MAXUI.oauth_grant_type = '%(oauth_grant_type)s';
+window._MAXUI.max_server = '%(max_server)s';
+window._MAXUI.max_server_alias = '%(max_server_alias)s';
+window._MAXUI.avatar_url = '%(avatar_url)s';
+window._MAXUI.profile_url = '%(profile_url)s'
+window._MAXUI.contexts = '%(contexts)s';
+window._MAXUI.activitySource = '%(activitySource)s';
+"""
+
+
+class communityVariables(grok.View):
+    grok.name('communityVariables.js')
+    grok.context(ICommunity)
+
+    def render(self):
+        self.request.response.addHeader('content-type', 'text/javascript;;charset=utf-8')
+        self.request.response.addHeader('cache-control', 'must-revalidate, max-age=0, no-cache, no-store')
+        portal_url = getSite().absolute_url()
+
+        registry = queryUtility(IRegistry)
+        settings = registry.forInterface(IMAXUISettings, check=False)
+
+        pm = getToolByName(self.context, "portal_membership")
+        if pm.isAnonymousUser():  # the user has not logged in
+            username = ''
+            oauth_token = ''
+        else:
+            member = pm.getAuthenticatedMember()
+            username = member.getUserName()
+            member = pm.getMemberById(username)
+            oauth_token = member.getProperty('oauth_token', None)
+
+        return TEMPLATE % dict(
+            username=username,
+            oauth_token=oauth_token,
+            oauth_grant_type=settings.oauth_grant_type,
+            max_server=settings.max_server,
+            max_server_alias=settings.max_server_alias,
+            avatar_url='%s/avatar/{0}' % (portal_url),
+            profile_url='%s/author/{0}' % (portal_url),
+            contexts=self.context.absolute_url(),
+            activitySource='activities'
+            )
--- ulearn/core/content/community.py
+++ ulearn/core/content/community.py
@@ -1,8 +1,19 @@
+# -*- coding: utf-8 -*-
 from five import grok
 from zope import schema
+from zope.component import queryUtility
+from zope.app.container.interfaces import IObjectAddedEvent
 
 from plone.directives import form, dexterity
 from plone.app.textfield import RichText
+from plone.z3cform.textlines.textlines import TextLinesFieldWidget
+from plone.registry.interfaces import IRegistry
+from plone.dexterity.utils import createContentInContainer
+
+from Products.CMFCore.utils import getToolByName
+
+from maxclient import MaxClient
+from mrs.max.browser.controlpanel import IMAXUISettings
 
 from ulearn.core import _
 
@@ -11,12 +22,67 @@ class ICommunity(form.Schema):
     """ A manageable community
     """
 
-    name = schema.TextLine(
+    title = schema.TextLine(
             title=_(u"Nom"),
             description=_(u"Nom de la comunitat"),
         )
 
-    # subscribed = schema.List(
-    #         title=_(u"Subscrits"),
-    #         description=_(u"Llista amb les persones subscrites"),
-    #     )
+    form.widget(subscribed=TextLinesFieldWidget)
+    subscribed = schema.List(
+            title=_(u"Subscrits"),
+            description=_(u"Llista amb les persones subscrites"),
+            value_type=schema.TextLine(),
+            required=False,
+            missing_value=[],
+        )
+
+
+class View(grok.View):
+    grok.context(ICommunity)
+
+    # def render(self):
+    #     return "asdasd"
+
+
+@grok.subscribe(ICommunity, IObjectAddedEvent)
+def initialize_community(community, event):
+    registry = queryUtility(IRegistry)
+    maxui_settings = registry.forInterface(IMAXUISettings)
+
+    maxclient = MaxClient(maxui_settings.max_server, maxui_settings.oauth_server)
+    maxclient.setActor(maxui_settings.max_restricted_username)
+    maxclient.setToken(maxui_settings.max_restricted_token)
+
+    # Add context for the community on MAX server
+    maxclient.addContext(community.absolute_url(),
+                         "Comunitat {}".format(community.title),
+                         dict(read='subscribed', write='subscribed', join='restricted', invite='restricted')
+                         )
+
+    # Subscribe owner
+    maxclient.subscribe(url=community.absolute_url(), username=community.Creator())
+
+    # Subscribe the invited users
+    for guest in community.subscribed:
+        maxclient.subscribe(url=community.absolute_url(), username=guest)
+
+    # Change workflow to intranet
+    portal_workflow = getToolByName(community, 'portal_workflow')
+    portal_workflow.doActionFor(community, 'publishtointranet')
+
+    # Disable Inheritance
+    community.__ac_local_roles_block__ = True
+
+    # Set permissions
+    for guest in community.subscribed:
+        community.manage_setLocalRoles(guest, ['Reader', 'Editor', 'Contributor'])
+
+    # Create default content
+    documents = createContentInContainer(community, 'Folder', title=u"Documents", checkConstraints=False)
+    enllacos = createContentInContainer(community, 'Folder', title=u"Enllaços", checkConstraints=False)
+    fotos = createContentInContainer(community, 'Folder', title=u"Fotos", checkConstraints=False)
+
+    # Change workflow to intranet
+    portal_workflow.doActionFor(documents, 'publishtointranet')
+    portal_workflow.doActionFor(enllacos, 'publishtointranet')
+    portal_workflow.doActionFor(fotos, 'publishtointranet')
