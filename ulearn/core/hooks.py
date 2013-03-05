b83db8dddb4f0fc60d9b9289fd50765998c5aae1
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-03-05 20:50:36 +0100

    Buttons in place, hooks as well

--- ulearn/core/hooks.py
+++ ulearn/core/hooks.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from Acquisition import aq_chain
 from zope.component import adapter
 from zope.component import queryUtility
 from zope.component.hooks import getSite
@@ -9,6 +10,7 @@ from plone.app.controlpanel.interfaces import IConfigurationChangedEvent
 from Products.CMFCore.utils import getToolByName
 
 from maxclient import MaxClient
+from ulearn.core.content.community import ICommunity
 from mrs.max.browser.controlpanel import IMAXUISettings
 
 
@@ -25,17 +27,21 @@ def Added(content, event):
 
     registry = queryUtility(IRegistry)
     settings = registry.forInterface(IMAXUISettings, check=False)
-    max_url = settings.max_server
+    effective_grant_type = settings.oauth_grant_type
 
     username = member.getUserName()
     memberdata = pm.getMemberById(username)
     oauth_token = memberdata.getProperty('oauth_token', None)
 
-    import ipdb;ipdb.set_trace()
-    community = content.newParent.absolute_url()
+    community = findContainerCommunity(content)
 
-    maxcli = MaxClient(max_url, actor=username, auth_method='oauth2')
-    maxcli.setOAuth2Auth(oauth_token, oauth2_scope='widgetcli')
+    if not community:
+        # For some reason the file we are creating is not inside a community
+        return
+
+    maxclient = MaxClient(url=settings.max_server, oauth_server=settings.oauth_server, grant_type=effective_grant_type)
+    maxclient.setActor(username)
+    maxclient.setToken(oauth_token)
 
     articles = dict(File=u'un', Image=u'una', Link=u'un')
 
@@ -48,9 +54,18 @@ def Added(content, event):
 
     activity_text = u'He afegit %(un)s %(type)s "%(name)s" a %(link)s' % parts
 
-    maxcli.addActivity(activity_text, contexts=[community, ])
+    maxclient.addActivity(activity_text, contexts=[community.absolute_url(), ])
 
 
+def findContainerCommunity(content):
+    for parent in aq_chain(content):
+        if ICommunity.providedBy(parent):
+            return parent
+
+    return None
+
+
+# not used
 @adapter(IConfigurationChangedEvent)
 def updateMAXUserInfo(event):
     """Subscriber que s'executa cada cop que canvien les preferencies d'usuari"""
