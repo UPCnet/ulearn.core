44c0b40751f1083652bc26c5a60f3577862200f4
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-07-04 18:15:33 +0200

    Transfer the MAX updater for user's profile subscriber to mrs.max.

--- ulearn/core/hooks.py
+++ ulearn/core/hooks.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from Acquisition import aq_chain
-from zope.component import adapter
 from zope.component import queryUtility
 from zope.component.hooks import getSite
 
 from plone.registry.interfaces import IRegistry
-from plone.app.controlpanel.interfaces import IConfigurationChangedEvent
+
 
 from Products.CMFCore.utils import getToolByName
 
@@ -78,30 +77,3 @@ def findContainerCommunity(content):
             return parent
 
     return None
-
-
-# not used
-@adapter(IConfigurationChangedEvent)
-def updateMAXUserInfo(event):
-    """Subscriber que s'executa cada cop que canvien les preferencies d'usuari"""
-    site = getSite()
-    pm = getToolByName(site, "portal_membership")
-    if pm.isAnonymousUser():  # the user has not logged in
-        username = ''
-        return
-    else:
-        member = pm.getAuthenticatedMember()
-
-    username = member.getUserName()
-    memberdata = pm.getMemberById(username)
-    properties = dict(displayName=memberdata.getProperty('fullname', None))
-
-    registry = queryUtility(IRegistry)
-    settings = registry.forInterface(IMAXUISettings, check=False)
-    max_url = settings.max_server
-
-    oauth_token = memberdata.getProperty('oauth_token', None)
-    maxcli = MaxClient(max_url, actor=username, auth_method='oauth2')
-    maxcli.setOAuth2Auth(oauth_token, oauth2_scope='widgetcli')
-
-    maxcli.modifyUser(username, properties)
