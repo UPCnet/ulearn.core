43ea4a75d432c79a5fa22617081d6bd06b234bbd
Corina Riba <corina.riba@upcnet.es>
2013-07-11 11:35:03 +0200

    Script generea .mo

---
+++ ulearn/core/locales/tranlations.sh
@@ -0,0 +1,3 @@
+msgfmt -o ca/LC_MESSAGES/ulearn.mo  ca/LC_MESSAGES/ulearn.po
+msgfmt -o es/LC_MESSAGES/ulearn.mo  es/LC_MESSAGES/ulearn.po
+msgfmt -o en/LC_MESSAGES/ulearn.mo  en/LC_MESSAGES/ulearn.po
