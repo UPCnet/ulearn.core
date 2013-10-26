d4a3b95e8ae40ac03b945bcfe9accd835266e1da
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-10-26 22:56:12 +0200

    Migration for the unified folder names.

--- ulearn/core/browser/migrations.py
+++ ulearn/core/browser/migrations.py
@@ -1,4 +1,6 @@
 from five import grok
+from Acquisition import aq_parent
+from Acquisition import aq_inner
 from zope.component import queryUtility
 from zope.component import getMultiAdapter
 from zope.component.hooks import getSite
@@ -8,6 +10,8 @@ from plone.portlets.interfaces import IPortletAssignmentMapping
 
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.interfaces import IPloneSiteRoot
+
+from ulearn.core.interfaces import IDocumentFolder, ILinksFolder, IPhotosFolder, IEventsFolder
 
 
 class portletfix(grok.View):
@@ -29,3 +33,22 @@ class portletfix(grok.View):
             target_manager_assignments = getMultiAdapter((community, target_manager), IPortletAssignmentMapping)
             for portlet in target_manager_assignments.keys():
                 del target_manager_assignments[portlet]
+
+
+class linkFolderFix(grok.View):
+    grok.context(IPloneSiteRoot)
+
+    def render(self):
+        portal = getSite()
+        pc = getToolByName(portal, "portal_catalog")
+        folder_ifaces = {IDocumentFolder.__identifier__: 'documents',
+                         ILinksFolder.__identifier__: 'links',
+                         IPhotosFolder.__identifier__: 'media',
+                         IEventsFolder.__identifier__: 'events'}
+
+        for iface in folder_ifaces.keys():
+            results = pc.searchResults(object_provides=iface)
+            for result in results:
+                parent = aq_parent(aq_inner(result.getObject()))
+                parent.manage_renameObjects((result.id,), (folder_ifaces[iface],))
+                print("renamed {} to {} in community {}".format(result.id, folder_ifaces[iface], parent))
