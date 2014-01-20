caf19a2ef908bfc28977d1fadf1b8bcb9289e964
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-01-20 11:52:53 +0100

    Las fixes to search views

--- ulearn/core/browser/searchuser.py
+++ ulearn/core/browser/searchuser.py
@@ -12,23 +12,27 @@ import random
 def searchUsersFunction(context, request, searchString):
     ignore = []
     mtool = getToolByName(context, 'portal_membership')
-    searchView = getMultiAdapter((aq_inner(context), request), name='pas_search')
-    # Si cal, per performance "max_results":20
-    userResults = searchView.merge(chain(*[searchView.searchUsers(**{field: searchString}) for field in ['name', 'fullname', 'email', 'twitter_username', 'ubicacio', 'location']]), 'userid')
-    userResults = [mtool.getMemberById(u['id']) for u in userResults if u['id'] not in ignore]
 
     # Get all my communities
-
     pc = getToolByName(context, 'portal_catalog')
     if ICommunity.providedBy(context):
-        visible = context.subscribed
+        visible = context.readers + context.subscribed + context.owners
     else:
+        # We are in root object
         my_communities = pc.searchResults(subscribed_users=mtool.getAuthenticatedMember().getId())
         visible = [user for community in my_communities for user in community.subscribed_users]
 
-    # Filter by community
-    userResults.sort(key=lambda x: x is not None and x.getProperty('fullname') is not None and normalizeString(x.getProperty('fullname')) or '')
+    if searchString:
+        searchView = getMultiAdapter((aq_inner(context), request), name='pas_search')
+        # Si cal, per performance "max_results":20
+        userResults = searchView.merge(chain(*[searchView.searchUsers(**{field: searchString}) for field in ['name', 'fullname', 'email', 'twitter_username', 'ubicacio', 'location']]), 'userid')
+        userResults = [mtool.getMemberById(u['id']) for u in userResults if u['id'] not in ignore]
+        userResults.sort(key=lambda x: x is not None and x.getProperty('fullname') is not None and normalizeString(x.getProperty('fullname')) or '')
+    else:
+        userResults = [mtool.getMemberById(user) for user in visible]
+        userResults.sort(key=lambda x: x is not None and x.getProperty('fullname') is not None and normalizeString(x.getProperty('fullname')) or '')
 
+    # Filter by community
     site = getSite()
     usersDict = []
     for user in userResults:
@@ -36,11 +40,11 @@ def searchUsersFunction(context, request, searchString):
         if user is not None and user.id in visible:
             usersDict.append({
                 'id': user.id,
-                'username': user.getProperty('fullname'),
-                'ubicacio': user.getProperty('ubicacio'),
-                'location': user.getProperty('location'),
-                'email': user.getProperty('email'),
-                'telefon': user.getProperty('telefon'),
+                'username': user.getProperty('fullname', ''),
+                'ubicacio': user.getProperty('ubicacio', ''),
+                'location': user.getProperty('location', ''),
+                'email': user.getProperty('email', ''),
+                'telefon': user.getProperty('telefon', ''),
                 'foto': str(mtool.getPersonalPortrait(user.id)),
                 'url': site.absolute_url() + '/profile/' + user.id
             })
