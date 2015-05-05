e88211f77bcdd2f81068049499d23bb9752c59b1
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-05-05 13:49:23 +0200

    Added experimental.securityindexing

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(name='ulearn.core',
           'Products.PloneFormGen',
           'collective.z3cform.datagridfield',
           'genweb.smartportlet',
+          'experimental.securityindexing'
       ],
       extras_require={'test': ['plone.app.testing',
                                'Products.PloneLDAP',
