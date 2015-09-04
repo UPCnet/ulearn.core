c612e6310879f686422748d8c167dacfc2067a6f
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-09-04 19:36:58 +0200

    CSS tests

--- ulearn/core/tests/test_viewlets.py
+++ ulearn/core/tests/test_viewlets.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-import unittest2 as unittest
-from zope.component import getUtility
-from zope.component import getMultiAdapter
 from ulearn.core.tests import uLearnTestBase
 from ulearn.core.testing import ULEARN_CORE_INTEGRATION_TESTING
+from ulearn.theme.browser.viewlets import gwCSSDevelViewlet
+from ulearn.theme.browser.viewlets import gwCSSProductionViewlet
 from ulearn.js.browser.viewlets import gwJSDevelViewlet
 from ulearn.js.browser.viewlets import gwJSProductionViewlet
 
@@ -20,9 +19,32 @@ class TestExample(uLearnTestBase):
         self.portal = self.layer['portal']
         self.request = self.layer['request']
 
+        ulearnthemeegg = pkg_resources.get_distribution('ulearn.theme')
         ulearnjsegg = pkg_resources.get_distribution('ulearn.js')
-        resource_file = open('{}/ulearn/core/config.json'.format(ulearnjsegg.location))
-        self.resources_conf = json.loads(resource_file.read())
+
+        resource_file_css = open('{}/ulearn/core/config.json'.format(ulearnthemeegg.location))
+        resource_file_js = open('{}/ulearn/core/config.json'.format(ulearnjsegg.location))
+        self.resources_conf_css = json.loads(resource_file_css.read())
+        self.resources_conf_js = json.loads(resource_file_js.read())
+
+    def test_css_development_resource_viewlet(self):
+
+        viewlet = gwCSSDevelViewlet(self.portal, self.request, None, None)
+        viewlet.update()
+        resources = viewlet.get_resources()
+
+        for resource in resources:
+            self.assertTrue('++' in resource)
+
+    def test_css_production_resource_viewlet(self):
+        viewlet = gwCSSProductionViewlet(self.portal, self.request, None, None)
+        viewlet.update()
+        resources = viewlet.get_resources()
+
+        for resource in resources:
+            self.assertTrue('++' in resource)
+
+        self.assertTrue(len(resources) == len(self.resources_conf_css['order']))
 
     def test_js_development_resource_viewlet(self):
 
@@ -41,4 +63,4 @@ class TestExample(uLearnTestBase):
         for resource in resources:
             self.assertTrue('++' in resource)
 
-        self.assertTrue(len(resources) == len(self.resources_conf['order']))
+        self.assertTrue(len(resources) == len(self.resources_conf_js['order']))
