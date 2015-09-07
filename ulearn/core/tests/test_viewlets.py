cc6d168dc457f3687dbdc0e9c4b4d471d1c06390
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-09-07 16:39:06 +0200

    Fix tests

--- ulearn/core/tests/test_viewlets.py
+++ ulearn/core/tests/test_viewlets.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 from ulearn.core.tests import uLearnTestBase
 from ulearn.core.testing import ULEARN_CORE_INTEGRATION_TESTING
-from ulearn.theme.browser.viewlets import gwCSSDevelViewlet
-from ulearn.theme.browser.viewlets import gwCSSProductionViewlet
-from ulearn.js.browser.viewlets import gwJSDevelViewlet
-from ulearn.js.browser.viewlets import gwJSProductionViewlet
+from ulearn.theme.browser.viewlets import gwCSSViewlet
+from ulearn.js.browser.viewlets import gwJSViewlet
 
 import json
 import pkg_resources
@@ -29,17 +27,17 @@ class TestExample(uLearnTestBase):
 
     def test_css_development_resource_viewlet(self):
 
-        viewlet = gwCSSDevelViewlet(self.portal, self.request, None, None)
+        viewlet = gwCSSViewlet(self.portal, self.request, None, None)
         viewlet.update()
-        resources = viewlet.get_resources()
+        resources = viewlet.get_development_resources()
 
         for resource in resources:
             self.assertTrue('++' in resource)
 
     def test_css_production_resource_viewlet(self):
-        viewlet = gwCSSProductionViewlet(self.portal, self.request, None, None)
+        viewlet = gwCSSViewlet(self.portal, self.request, None, None)
         viewlet.update()
-        resources = viewlet.get_resources()
+        resources = viewlet.get_production_resources()
 
         for resource in resources:
             self.assertTrue('++' in resource)
@@ -48,17 +46,17 @@ class TestExample(uLearnTestBase):
 
     def test_js_development_resource_viewlet(self):
 
-        viewlet = gwJSDevelViewlet(self.portal, self.request, None, None)
+        viewlet = gwJSViewlet(self.portal, self.request, None, None)
         viewlet.update()
-        resources = viewlet.get_resources()
+        resources = viewlet.get_development_resources()
 
         for resource in resources:
             self.assertTrue('++' in resource)
 
     def test_js_production_resource_viewlet(self):
-        viewlet = gwJSProductionViewlet(self.portal, self.request, None, None)
+        viewlet = gwJSViewlet(self.portal, self.request, None, None)
         viewlet.update()
-        resources = viewlet.get_resources()
+        resources = viewlet.get_production_resources()
 
         for resource in resources:
             self.assertTrue('++' in resource)
