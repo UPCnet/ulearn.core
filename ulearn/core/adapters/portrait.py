e8b221522f6b7ed03f245a4e54b3a5a3b2f381d2
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-11-04 16:45:15 +0100

    New pluggable changeMemberPortrait more specific for ulearn

---
+++ ulearn/core/adapters/portrait.py
@@ -0,0 +1,59 @@
+from cStringIO import StringIO
+from OFS.Image import Image
+from PIL import ImageOps
+from five import grok
+from Products.CMFCore.utils import getToolByName
+from Products.PlonePAS.interfaces.membership import IMembershipTool
+from genweb.core.adapters.portrait import IPortraitUploadAdapter
+from mrs.max.utilities import IMAXClient
+from ulearn.theme.browser.interfaces import IUlearnTheme
+from zope.component import getUtility
+
+import PIL
+
+
+@grok.implementer(IPortraitUploadAdapter)
+@grok.adapter(IMembershipTool, IUlearnTheme)
+class PortraitUploadAdapter(object):
+    """ Default adapter for portrait custom actions """
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self, portrait, safe_id):
+        if portrait and portrait.filename:
+            # scaled, mimetype = scale_image(portrait, max_size=(250, 250))
+            scaled, mimetype = convertSquareImage(portrait)
+            portrait = Image(id=safe_id, file=scaled, title='')
+            membertool = getToolByName(self.context, 'portal_memberdata')
+            membertool._setPortrait(portrait, safe_id)
+
+            # Update the user's avatar on MAX
+            # the next line to user's that have '-' in id
+            safe_id = safe_id.replace('--', '-')
+            scaled.seek(0)
+
+            # Upload to MAX server using restricted user credentials
+            maxclient, settings = getUtility(IMAXClient)()
+            maxclient.setActor(settings.max_restricted_username)
+            maxclient.setToken(settings.max_restricted_token)
+            maxclient.people[safe_id].avatar.post(upload_file=scaled)
+
+
+def convertSquareImage(image_file):
+    CONVERT_SIZE = (250, 250)
+    image = PIL.Image.open(image_file)
+    format = image.format
+    mimetype = 'image/%s' % format.lower()
+
+    # Old way
+    # if image.size[0] > 250 or image.size[1] > 250:
+    #     image.thumbnail((250, 9800), PIL.Image.ANTIALIAS)
+    #     image = image.transform((250, 250), PILImage.EXTENT, (0, 0, 250, 250), PILImage.BICUBIC)
+
+    result = ImageOps.fit(image, CONVERT_SIZE, method=PIL.Image.ANTIALIAS, centering=(0, 0))
+    new_file = StringIO()
+    result.save(new_file, format, quality=88)
+    new_file.seek(0)
+
+    return new_file, mimetype
