c7745e0218360f1c1c6b62d45941ae9bd4ac13f4
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-12-23 21:27:50 +0100

    New viewlet to hold directives related with Angular state variables used by services

--- ulearn/core/browser/viewlets.py
+++ ulearn/core/browser/viewlets.py
@@ -1,9 +1,14 @@
 from five import grok
 from hashlib import sha1
+from plone import api
+from zope.interface import Interface
 from plone.app.layout.viewlets.interfaces import IPortalHeader
 
 from genweb.core.gwuuid import IGWUUID
 from ulearn.core.content.community import ICommunity
+from ulearn.core import _
+
+import json
 
 
 class CommunityNGDirective(grok.Viewlet):
@@ -16,3 +21,15 @@ class CommunityNGDirective(grok.Viewlet):
         self.community_gwuuid = IGWUUID(self.context).get()
         self.community_url = self.context.absolute_url()
         self.community_type = self.context.community_type
+
+
+class ULearnNGDirectives(grok.Viewlet):
+    grok.context(Interface)
+    grok.name('ulearn.ulearnngdirectives')
+    grok.viewletmanager(IPortalHeader)
+
+    def get_communities(self):
+        pc = api.portal.get_tool('portal_catalog')
+        all_communities = [{'hash': 'all', 'title': _(u'Todas las comunidades')}]
+        all_communities += [{'hash': community.community_hash, 'title': community.Title} for community in pc.searchResults(portal_type='ulearn.community')]
+        return json.dumps(all_communities)
