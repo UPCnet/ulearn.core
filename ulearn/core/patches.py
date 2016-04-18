367fd9f47b99ff80e179f782b0ee12fb59ef2084
Paco Gregori <francisco.gregori@upcnet.es>
2016-04-18 16:09:56 +0200

    add patch when remove user on userpref, remove on max too

--- ulearn/core/patches.py
+++ ulearn/core/patches.py
@@ -6,6 +6,12 @@ from zope.component import getUtility
 from zope.component import getUtilitiesFor
 from souper.interfaces import ICatalogFactory
 
+from Acquisition import aq_inner
+from zExceptions import Forbidden
+from Products.CMFCore.interfaces import ISiteRoot
+from Products.CMFCore.utils import getToolByName
+from mrs.max.utilities import IMAXClient
+
 
 # We are patching the enumerateUsers method of the mutable_properties plugin to
 # make it return all the available user properties extension
@@ -54,3 +60,47 @@ def enumerateUsers(self, id=None, login=None, exact_match=False, **kw):
                 users_profile.append(user_dict)
 
         return tuple(users_profile)
+
+
+def deleteMembers(self, member_ids):
+    # this method exists to bypass the 'Manage Users' permission check
+    # in the CMF member tool's version
+    context = aq_inner(self.context)
+    mtool = getToolByName(self.context, 'portal_membership')
+    # Delete members in acl_users.
+    acl_users = context.acl_users
+    if isinstance(member_ids, basestring):
+        member_ids = (member_ids,)
+    member_ids = list(member_ids)
+    for member_id in member_ids[:]:
+        member = mtool.getMemberById(member_id)
+        if member is None:
+            member_ids.remove(member_id)
+        else:
+            if not member.canDelete():
+                raise Forbidden
+            if 'Manager' in member.getRoles() and not self.is_zope_manager:
+                raise Forbidden
+    try:
+        acl_users.userFolderDelUsers(member_ids)
+    except (AttributeError, NotImplementedError):
+        raise NotImplementedError('The underlying User Folder '
+                                  'doesn\'t support deleting members.')
+
+    self.maxclient, self.settings = getUtility(IMAXClient)()
+    self.maxclient.setActor(self.settings.max_restricted_username)
+    self.maxclient.setToken(self.settings.max_restricted_token)
+    # Delete member data in portal_memberdata.
+    mdtool = getToolByName(context, 'portal_memberdata', None)
+    if mdtool is not None:
+        for member_id in member_ids:
+            mdtool.deleteMemberData(member_id)
+            try:
+                self.maxclient.people[member_id].delete()
+            except:
+                # No existe el usuari en max
+                pass
+
+    # Delete members' local roles.
+    mtool.deleteLocalRoles(getUtility(ISiteRoot), member_ids,
+                           reindex=1, recursive=1)
