b187fed472fb471c088b5533b1b9e7b30477bb38
Paco Gregori <francisco.gregori@upcnet.es>
2016-07-01 09:31:37 +0200

    tranlates

--- ulearn/core/locales/update.sh
+++ ulearn/core/locales/update.sh
@@ -2,16 +2,16 @@
 cd ..
 cd ..
 cd ..
-/var/plone/genweb.rectorat/bin/i18ndude rebuild-pot --pot ulearn/core/locales/ulearn.pot --create ulearn ../ulearn.theme  .
+/var/plone/ploneweb/comunitats.buildout/bin/i18ndude rebuild-pot --pot ulearn/core/locales/ulearn.pot --create ulearn ../ulearn.theme  .
 cd ulearn/core/locales/ca/LC_MESSAGES
-/var/plone/genweb.rectorat/bin/i18ndude sync --pot ../../ulearn.pot ulearn.po
+/var/plone/ploneweb/comunitats.buildout/bin/i18ndude sync --pot ../../ulearn.pot ulearn.po
 cd ..
 cd ..
 cd en
 cd LC_MESSAGES
-/var/plone/genweb.rectorat/bin/i18ndude sync --pot ../../ulearn.pot ulearn.po
+/var/plone/ploneweb/comunitats.buildout/bin/i18ndude sync --pot ../../ulearn.pot ulearn.po
 cd ..
 cd ..
 cd es
 cd LC_MESSAGES
-/var/plone/genweb.rectorat/bin/i18ndude sync --pot ../../ulearn.pot ulearn.po
+/var/plone/ploneweb/comunitats.buildout/bin/i18ndude sync --pot ../../ulearn.pot ulearn.po
