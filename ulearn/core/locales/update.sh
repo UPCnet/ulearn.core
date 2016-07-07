fa1681c7d04ea05d8d3c1ee33ee74c1d277e749c
Pilar Marinas <pilar.marinas@upcnet.es>
2016-07-07 09:31:18 +0200

    Translate user properties default

--- ulearn/core/locales/update.sh
+++ ulearn/core/locales/update.sh
@@ -2,16 +2,16 @@
 cd ..
 cd ..
 cd ..
-/var/plone/ploneweb/comunitats.buildout/bin/i18ndude rebuild-pot --pot ulearn/core/locales/ulearn.pot --create ulearn ../ulearn.theme  .
+/var/plone/ulearn.externsV2/bin/i18ndude rebuild-pot --pot ulearn/core/locales/ulearn.pot --create ulearn ../ulearn.theme  .
 cd ulearn/core/locales/ca/LC_MESSAGES
-/var/plone/ploneweb/comunitats.buildout/bin/i18ndude sync --pot ../../ulearn.pot ulearn.po
+/var/plone/ulearn.externsV2/bin/i18ndude sync --pot ../../ulearn.pot ulearn.po
 cd ..
 cd ..
 cd en
 cd LC_MESSAGES
-/var/plone/ploneweb/comunitats.buildout/bin/i18ndude sync --pot ../../ulearn.pot ulearn.po
+/var/plone/ulearn.externsV2/bin/i18ndude sync --pot ../../ulearn.pot ulearn.po
 cd ..
 cd ..
 cd es
 cd LC_MESSAGES
-/var/plone/ploneweb/comunitats.buildout/bin/i18ndude sync --pot ../../ulearn.pot ulearn.po
+/var/plone/ulearn.externsV2/bin/i18ndude sync --pot ../../ulearn.pot ulearn.po
