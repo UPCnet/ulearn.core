94d698d94652e8e86a685124990baf09005cff50
Pilar Marinas <pilar.marinas@upcnet.es>
2017-04-11 14:52:17 +0200

    Patches Notify LocalrolesModifiedEvent

--- ulearn/core/patches.py
+++ ulearn/core/patches.py
@@ -22,7 +22,6 @@ from genweb.core.gwuuid import IGWUUID
 import logging
 logger = logging.getLogger('event.LDAPMultiPlugin')
 
-
 # We are patching the enumerateUsers method of the mutable_properties plugin to
 # make it return all the available user properties extension
 def enumerateUsers(self, id=None, login=None, exact_match=False, **kw):
@@ -234,3 +233,66 @@ def authenticateCredentials(self, credentials):
 
     logger.error('XXX Successful login of {}'.format(login))
     return (user.getId(), user.getUserName())
+
+
+
+from zope.event import notify
+from plone.app.workflow.events import LocalrolesModifiedEvent
+from Products.statusmessages.interfaces import IStatusMessage
+from plone.app.workflow import PloneMessageFactory as _
+
+# Notify LocalrolesModifiedEvent if settings
+# plone.app.workflow.browser.sharing.SharingView
+def handle_form(self):
+        """
+        We split this out so we can reuse this for ajax.
+        Will return a boolean if it was a post or not
+        """
+        postback = True
+
+        form = self.request.form
+        submitted = form.get('form.submitted', False)
+        save_button = form.get('form.button.Save', None) is not None
+        cancel_button = form.get('form.button.Cancel', None) is not None
+
+        if submitted and save_button and not cancel_button:
+            if not self.request.get('REQUEST_METHOD', 'GET') == 'POST':
+                raise Forbidden
+
+            authenticator = self.context.restrictedTraverse('@@authenticator',
+                                                            None)
+            if not authenticator.verify():
+                raise Forbidden
+
+            # Update the acquire-roles setting
+            if self.can_edit_inherit():
+                inherit = bool(form.get('inherit', False))
+                reindex = self.update_inherit(inherit, reindex=False)
+            else:
+                reindex = False
+
+            # Update settings for users and groups
+            entries = form.get('entries', [])
+            roles = [r['id'] for r in self.roles()]
+            settings = []
+            for entry in entries:
+                settings.append(
+                    dict(id=entry['id'],
+                         type=entry['type'],
+                         roles=[r for r in roles
+                            if entry.get('role_%s' % r, False)]))
+            if settings:
+                reindex = self.update_role_settings(settings, reindex=False) \
+                            or reindex
+                notify(LocalrolesModifiedEvent(self.context, self.request))
+            if reindex:
+                self.context.reindexObjectSecurity()
+                notify(LocalrolesModifiedEvent(self.context, self.request))
+            IStatusMessage(self.request).addStatusMessage(
+                _(u"Changes saved."), type='info')
+
+        # Other buttons return to the sharing page
+        if cancel_button:
+            postback = False
+
+        return postback
