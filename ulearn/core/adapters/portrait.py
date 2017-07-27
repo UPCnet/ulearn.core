99ebcc5f5abe6ee1d35d726886c29e1316120dae
Roberto Diaz <roberto.diaz@upcnet.es>
2017-07-27 13:25:26 +0200

    bypass admin error with no photo found

--- ulearn/core/adapters/portrait.py
+++ ulearn/core/adapters/portrait.py
@@ -27,28 +27,35 @@ class PortraitUploadAdapter(object):
         if portrait and portrait.filename:
             # scaled, mimetype = scale_image(portrait, max_size=(250, 250))
             scaled, mimetype = convertSquareImage(portrait)
-            portrait = Image(id=safe_id, file=scaled, title='')
-            membertool = getToolByName(self.context, 'portal_memberdata')
-            membertool._setPortrait(portrait, safe_id)
+            if scaled:
+                portrait = Image(id=safe_id, file=scaled, title='')
+                membertool = getToolByName(self.context, 'portal_memberdata')
+                membertool._setPortrait(portrait, safe_id)
 
-            # Update the user's avatar on MAX
-            # the next line to user's that have '-' in id
-            safe_id = safe_id.replace('--', '-')
-            scaled.seek(0)
+                # Update the user's avatar on MAX
+                # the next line to user's that have '-' in id
+                safe_id = safe_id.replace('--', '-')
+                scaled.seek(0)
 
-            # Upload to MAX server using restricted user credentials
-            maxclient, settings = getUtility(IMAXClient)()
-            maxclient.setActor(settings.max_restricted_username)
-            maxclient.setToken(settings.max_restricted_token)
-            try:
-                maxclient.people[safe_id].avatar.post(upload_file=scaled)
-            except Exception as exc:
-                logger.error(exc.message)
+                # Upload to MAX server using restricted user credentials
+                maxclient, settings = getUtility(IMAXClient)()
+                maxclient.setActor(settings.max_restricted_username)
+                maxclient.setToken(settings.max_restricted_token)
+                try:
+                    maxclient.people[safe_id].avatar.post(upload_file=scaled)
+                except Exception as exc:
+                    logger.error(exc.message)
 
 
 def convertSquareImage(image_file):
     CONVERT_SIZE = (250, 250)
-    image = PIL.Image.open(image_file)
+    try:
+        image = PIL.Image.open(image_file)
+    except:
+        new_file = None
+        mimetype = 'image/jpg'
+        return new_file, mimetype
+
     format = image.format
     mimetype = 'image/%s' % format.lower()
 
