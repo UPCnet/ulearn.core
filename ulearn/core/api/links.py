e475e84825a93cdc83219899dad2aae73553f7bb
Roberto Diaz <roberto.diaz@upcnet.es>
2017-09-14 11:45:56 +0200

    updated links APi to remove ?language pattern

--- ulearn/core/api/links.py
+++ ulearn/core/api/links.py
@@ -9,50 +9,64 @@ from ulearn.core.api.root import APIRoot
 from zope.component import getUtility
 from plone.registry.interfaces import IRegistry
 from ulearn.core.controlpanel import IUlearnControlPanelSettings
+from ulearn.core.api import ObjectNotFound
 
 
 class Links(REST):
     """
         /api/links
-
-        http://localhost:8090/Plone/api/links/?language=es
-
     """
+    placeholder_type = 'link'
+    placeholder_id = 'language'
 
     grok.adapts(APIRoot, IPloneSiteRoot)
     grok.require('genweb.authenticated')
 
+
+class Link(REST):
+    """
+        /api/links/{language}
+
+        http://localhost:8090/Plone/api/links/es
+
+    """
+    grok.adapts(Links, IPloneSiteRoot)
+    grok.require('genweb.authenticated')
+
     @api_resource(required=['language'])
     def GET(self):
-        """ Return the links from fixed gestion folder and ControlPanel """
+        """ Return the links from Menu Gestion folder and Menu ControlPanel """
         language = self.params['language']
         portal = api.portal.get()
-        path = portal['gestion']['menu'][language]  # fixed en code... always in this path
-        # roles = api.user.get_roles(username=username, obj=path)
-        folders = api.content.find(context=path, depth=1)
-        results = {}
-        # Links from gestion folder
-        for folder in folders:
-            results[folder.Title] = []
-            menufolder = folder.getObject().items()
-            for item in menufolder:
-                menuLink = dict(remote=item[1].remoteUrl,
-                                title=item[1].title,
-                                )
-                results[folder.Title].append(menuLink)
+        try:
+            path = portal['gestion']['menu'][language]  # fixed en code... always in this path
+            folders = api.content.find(context=path, depth=1)
 
-        registry = getUtility(IRegistry)
-        settings = registry.forInterface(IUlearnControlPanelSettings, check=False)
+            # Links from gestion folder
+            resultsGestion = {}
+            for folder in folders:
+                resultsGestion[folder.Title] = []
+                menufolder = folder.getObject().items()
+                for item in menufolder:
+                    menuLink = dict(url=item[1].remoteUrl,
+                                    title=item[1].title,
+                                    )
+                    resultsGestion[folder.Title].append(menuLink)
 
-        results2 = []
-        # Links from controlpanel
-        if settings.quicklinks_table is not None:
-            for item in settings.quicklinks_table:
-                quickLink = dict(title=item['text'],
-                                 remote=item['link'],
-                                 icon=item['icon']
-                                 )
-            results2.append(quickLink)
-        values = {'menu_gestion': results, 'menu_carpetas': results2}
+            registry = getUtility(IRegistry)
+            settings = registry.forInterface(IUlearnControlPanelSettings, check=False)
 
-        return ApiResponse(values)
+            # Links from controlpanel
+            resultsControlPanel = []
+            if settings.quicklinks_table is not None:
+                for item in settings.quicklinks_table:
+                    quickLink = dict(title=item['text'],
+                                     url=item['link'],
+                                     icon=item['icon']
+                                     )
+                resultsControlPanel.append(quickLink)
+            values = {'Menu_Gestion': resultsGestion, 'Menu_Controlpanel': resultsControlPanel}
+
+            return ApiResponse(values)
+        except:
+            raise ObjectNotFound('Language not configured in this Site.')
